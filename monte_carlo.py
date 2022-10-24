d9de23e41c0a3634c466dc41594052140d6b1773
Eirik Aalstad Baekkelund <89584092+eirikbaekkelund@users.noreply.github.com>
2022-10-24 16:08:28 +0100

    Update plot function and MCMC to Monte Carlo class

--- monte_carlo.py
+++ monte_carlo.py
@@ -3,7 +3,6 @@ import torch.distributions as dist
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-from IPython.display import display, Latex
 
 class MonteCarlo:
     
@@ -35,7 +34,7 @@ class MonteCarlo:
         
         # P(X|mu) assuming independence
         log_posterior = dist.Bernoulli(mu).log_prob(X).sum(0)
-        
+        # log[ P(mu)P(X|mu) ]
         return log_prior + log_posterior
     
     def generate_samples(self, n_samples, n_observed, distribution):
@@ -115,3 +114,86 @@ class MonteCarlo:
         """
         # P(X < p)
         return self.weights @ (self.samples < percentage).float()
+    
+    def markov_chain_step(self, current_mu, observed_data, proposal_std):
+        """
+        Markov Chain Monte Carlo step-wise function.
+        Accepts a new value 
+        """
+        proposal_Q = dist.Normal(loc=current_mu, 
+                                 scale=proposal_std)
+        sample_x_prime = proposal_Q.sample()
+
+
+        try:
+            prob_sample_joint = self.log_joint_bernoulli(mu=sample_x_prime,
+                                                         X=observed_data)
+        except ValueError:
+            prob_sample_joint = -torch.inf
+        
+        prob_current_joint = self.log_joint_bernoulli(mu=current_mu,
+                                                      X=observed_data)
+        
+        if prob_sample_joint - prob_current_joint > torch.rand(1).log().item():
+            return sample_x_prime, 1
+        else:
+            return current_mu, 0
+    
+    def run_markov_chain(self, init, num_steps, proposal_std):
+        """
+        Computes a sequence of samples and the acceptance rate
+
+        Parameters:
+        init - guessed initial value to start at
+        num_steps - number of times to run MCMC
+        proposal_std - suggested variance for the normal distribution
+
+        Returns:
+        mu_chain - array of suggested values for mu in Normal from MCMC
+        acceptance_rate - proportion of suggested samples accepted
+        """
+
+        self.mu_chain = [torch.scalar_tensor(init)]
+        n_accepted = 0
+
+        for _ in range(num_steps):
+            mu, accept_bool = self.markov_chain_step(self.mu_chain[-1], 
+                                                self.observed_toss,
+                                                proposal_std=proposal_std)
+            self.mu_chain.append(mu)
+            n_accepted += accept_bool
+        
+        self.acceptance_rate = n_accepted / num_steps
+        
+        return self.mu_chain, self.acceptance_rate
+    
+    def plot_chain_steps(self):
+        
+        plt.plot(self.mu_chain, label='$\mu$ samples')
+        plt.title(f"MCMC Acceptance Rate: {self.acceptance_rate}")
+        plt.ylim(0, 1);
+        plt.xlabel("MCMC iteration")
+        plt.ylabel('$\mu$');
+        plt.show()
+    
+    def plot_posterior(self):
+        domain = np.linspace(0,1, 300)
+        posterior = self.log_joint_bernoulli(mu=torch.FloatTensor(domain), 
+                                             X=self.observed_toss[:,None]).exp()
+
+        plt.plot(domain, posterior, linestyle='--', label='$P(\mu | X)$')
+        plt.axvline(x=np.mean(self.mu_chain), color='red', label='MCMC $\mu$')
+        plt.axvline(x=np.mean(self.observed_toss.numpy()), color= 'black', label='Observed $\mu$')
+        plt.xlim(0, 1)
+        plt.xlabel("$\mu$")
+        plt.ylabel("$p(\mu | x_1,\dots,x_N)$")
+        plt.title("MCMC histogram of posterior");
+        plt.legend()
+        plt.show()
+
+        
+
+        
+        
+        
+        
